010dda4bb54556f13f638912ee191a6a4ed8a7f1
SangHyun, Kim <shubbydobob@gmail.com>
2025-08-10 18:21:31 +0900

    [Bronze V] Title: 나머지, Time: 92 ms, Memory: 12884 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -2,15 +2,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc  = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int C = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            int C = sc.nextInt();
 
-        System.out.println((A+B)%C);
-        System.out.println(((A%C)+(B%C))%C);
-        System.out.println((A*B)%C);
-        System.out.println(((A%C)*(B%C))%C);
+           int[] results = computedModIdentities(A, B, C);
+           StringBuilder sb = new StringBuilder();
+           for (int r: results) sb.append(r).append('\n');
+           System.out.print(sb);
+        }
+    }
 
+    static int[] computedModIdentities(int A, int B, int C){
+        int aMod = A % C;
+        int bMod = B % C;
+
+        int r1 = (A + B) % C;
+        int r2 = (aMod + bMod) % C;
+        int r3 = (A * B) % C;
+        int r4 = (aMod * bMod) % C;
+
+        return new int[] {r1, r2, r3, r4};
     }
 }
