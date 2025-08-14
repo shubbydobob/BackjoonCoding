db8be9812c11110360e5f28b9822d29250c09871
SangHyun, Kim <shubbydobob@gmail.com>
2025-08-14 19:45:52 +0900

    [Bronze V] Title: 고양이, Time: 60 ms, Memory: 11368 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        String A = "\\    /\\";
+        String B = " )  ( ')";
+        String C = "(  /  )";
+        String D = " \\(__)|";
+
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+        System.out.println(D);
+    }
+}
