4170033ea51e7f71daf2459463cd7b07962b7dac
SangHyun, Kim <shubbydobob@gmail.com>
2025-08-27 21:37:25 +0900

    [Bronze III] Title: 최소, 최대, Time: 1360 ms, Memory: 308876 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] A = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        int min = A[0];
+        int max = A[0];
+
+        for (int i = 0; i < N; i++) {
+            if (A[i] < min) {
+                min = A[i];
+            }
+            if (A[i] > max) {
+                max = A[i];
+            }
+        }
+        System.out.println(min + " " + max);
+        sc.close();
+    }
+}
