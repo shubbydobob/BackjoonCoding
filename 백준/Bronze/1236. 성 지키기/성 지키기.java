37e04f3e95fb1495cb9fe126b07914ad62cbf8c1
SangHyun, Kim <shubbydobob@gmail.com>
2025-09-28 18:59:20 +0900

    [Bronze I] Title: 성 지키기, Time: 100 ms, Memory: 12964 KB -BaekjoonHub

---
+++ 백준/Bronze/1236. 성 지키기/성 지키기.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+       int N = sc.nextInt();
+       int M = sc.nextInt();
+
+       char[][] map = new char[N][M];
+       for(int i = 0; i<N; i++)
+           map[i] = sc.next().toCharArray();
+
+       int existRowCount = 0;
+       for (int r =0; r< N; r++) {
+           boolean exist = false;
+           for(int c = 0; c < M; c++)
+               if(map[r][c] == 'X') {
+                   exist = true;
+                   break;
+               }
+           if(exist) existRowCount++;
+       }
+
+       int existColCount = 0;
+       for (int c= 0; c < M; c++) {
+           boolean exist = false;
+           for (int r = 0; r < N; r++)
+               if(map[r][c] == 'X') {
+                   exist = true;
+                   break;
+               }
+           if(exist) existColCount++;
+
+       }
+
+       int needRowCount = N - existRowCount;
+       int needColCount = M - existColCount;
+
+        System.out.println(Math.max(needRowCount, needColCount));
+
+        sc.close();
+    }
+}
