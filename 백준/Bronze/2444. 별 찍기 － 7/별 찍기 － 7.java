e4218cf47db131eb4da304a431b34958bc0b5c77
SangHyun, Kim <shubbydobob@gmail.com>
2025-10-12 18:25:31 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 180 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -7,22 +7,10 @@ public class Main {
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 1; j <= 2 * i - 1; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+            System.out.println(" ".repeat(n - i) + "*".repeat(2 * i - 1));
         }
-        for (int i = n - 1; i >= 1; i--) {
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 1; j <= 2 * i - 1; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+        for (int i = n -1; i >= 1; i--) {
+            System.out.println(" ".repeat(n - i) + "*".repeat(2 * i - 1));
         }
 
         sc.close();
