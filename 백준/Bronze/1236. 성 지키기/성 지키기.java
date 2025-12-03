cf20a5c2caaf49bf2feaa63df16894045ce488a1
SangHyun, Kim <shubbydobob@gmail.com>
2025-12-03 20:33:13 +0900

    [Bronze I] Title: 성 지키기, Time: 96 ms, Memory: 12928 KB -BaekjoonHub

--- 백준/Bronze/1236. 성 지키기/성 지키기.java
+++ 백준/Bronze/1236. 성 지키기/성 지키기.java
@@ -4,41 +4,38 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-       int N = sc.nextInt();
-       int M = sc.nextInt();
+        int N = sc.nextInt();
+        int M = sc.nextInt();
 
-       char[][] map = new char[N][M];
-       for(int i = 0; i<N; i++)
-           map[i] = sc.next().toCharArray();
+        char[][] map = new char[N][M];
+        for(int i = 0; i < N; i++)
+            map[i] = sc.next().toCharArray();
 
-       int existRowCount = 0;
-       for (int r =0; r< N; r++) {
-           boolean exist = false;
-           for(int c = 0; c < M; c++)
-               if(map[r][c] == 'X') {
-                   exist = true;
-                   break;
-               }
-           if(exist) existRowCount++;
-       }
+        int existRowCount = 0;
+        for(int i = 0; i < N; i++) {
+            boolean exist = false;
+            for(int j = 0; j < M; j++)
+                if(map[i][j] == 'X') {
+                    exist = true;
+                    break;
+                }
+            if(exist) existRowCount++;
+        }
 
-       int existColCount = 0;
-       for (int c= 0; c < M; c++) {
-           boolean exist = false;
-           for (int r = 0; r < N; r++)
-               if(map[r][c] == 'X') {
-                   exist = true;
-                   break;
-               }
-           if(exist) existColCount++;
+        int existColumnCount = 0;
+        for(int i = 0; i < M; i++) {
+            boolean exist = false;
+            for(int j = 0; j < N; j++)
+                if(map[j][i] == 'X') {
+                    exist = true;
+                    break;
+                }
+            if(exist) existColumnCount++;
+        }
 
-       }
+        int needRowCount = N - existRowCount;
+        int needColumnCount = M - existColumnCount;
 
-       int needRowCount = N - existRowCount;
-       int needColCount = M - existColCount;
-
-        System.out.println(Math.max(needRowCount, needColCount));
-
-        sc.close();
+        System.out.println(Math.max(needRowCount, needColumnCount));
     }
 }
